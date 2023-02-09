9a95e893b744a7d6d4a7df40cdc66e15e1bd54bc
unknown <arbef161@gmail.com>
2023-02-09 13:29:40 -0500

    using dataframes instead of series

--- test.py
+++ test.py
@@ -8,22 +8,56 @@ import argparse
 import pandas as pd
 
 path = 'list.csv'
+
 parser = argparse.ArgumentParser(description='build and alter database')
 parser.add_argument('-d', '--delete', action='store_true')
-
-header = ['Name', 'Year Formed']
 args = parser.parse_args()
 
 if args.delete == True:
     print('Resetting CSV file')
     os.remove(path)
 
+dict_toAdd = {'Name': [], 'Year Formed':[]}
 
 #check if file exists
 #check if value already exists
 if os.path.exists('list.csv'):
     print('file exists')
-    #n_args = len(sys.argv)
+    df_original = pd.read_csv(path)
+    print(df_original)
+else:
+    print('creating new csv')
+    df_original = pd.DataFrame(dict_toAdd)
+    print(df_original)
+
+name_toAdd = input('Please input a name to add: ')
+if name_toAdd not in df_original.Name.values:
+    print('Name: %s is not in file' % name_toAdd)
+    dict_toAdd['Name'].append(name_toAdd)
+    year_formed = int(input('Please enter and integer for the year formed: '))
+    dict_toAdd['Year Formed'].append(year_formed)
+    print(dict_toAdd)
+else:
+    print('Name: %s already in file. Exiting...' % name_toAdd)
+    exit()
+
+df_toAdd = pd.DataFrame(dict_toAdd)
+print(df_toAdd)
+ret = pd.concat([df_original, df_toAdd])
+print(ret)
+#ret.reset_index(drop=True)
+print(ret)
+
+csv_data = ret.to_csv(path)
+#with open('list.csv', 'a') as fp:
+#    fp.writelines(value + '\n' for value in missing)
+
+#df = pd.DataFrame(my_df)
+#my_df = pd.Series(header)
+#csv_data = my_df.to_csv('list.csv')
+#with open('list.csv', 'w') as fp:
+#    fp.writelines(header)    
+# #n_args = len(sys.argv)
     #if n_args == 1:
     #    print('please include name')
     #    sys.exit()        
@@ -35,35 +69,3 @@ if os.path.exists('list.csv'):
     
     #with open('list.csv', 'r') as fp:
     #    s = fp.read()
-    df = pd.read_csv(path)
-    missing = []
-    name_toAdd = input('Please input a name to add')
-    
-    if name_toAdd not in df.Name.values:
-        print('Name: %s is not in file' % name_toAdd)
-        missing.append(name_toAdd)
-        year_formed = int(input('Please enter and integer for the year formed'))
-        missing.append(year_formed)
-        print(missing)
-    else:
-        print('Name: %s already in file. Exiting...' % name_toAdd)
-        exit()
-    series_toAdd = {}
-    series_toAdd['Band'] = name_toAdd
-    series_toAdd['Year Formed'] = year_formed
-    ser = pd.Series(series_toAdd)
-    pd.concat([df, ser])
-    #df.loc[len(df)] = missing
-    csv_data = df.to_csv(path)
-    #with open('list.csv', 'a') as fp:
-    #    fp.writelines(value + '\n' for value in missing)
-else: 
-    print('creating new csv')
-    data = {'Name':['mbv', 'slowdive'], 'Year Formed':[2002, 3112]}
-    df = pd.DataFrame(data)
-    csv_data = df.to_csv(path)
-    #df = pd.DataFrame(my_df)
-    #my_df = pd.Series(header)
-    #csv_data = my_df.to_csv('list.csv')
-    #with open('list.csv', 'w') as fp:
-    #    fp.writelines(header)
